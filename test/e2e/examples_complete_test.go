455663eb85f57a00638fdc64c81c561659577691
zjhe <hezijie@microsoft.com>
2022-03-04 17:50:22 +0800

    change e2e test to use test_helper function

--- test/e2e/examples_complete_test.go
+++ test/e2e/examples_complete_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	test_helper "github.com/lonegunmanb/terraform-module-test-helper"
 	"github.com/stretchr/testify/assert"
 )
@@ -20,21 +19,11 @@ func TestExamplesComplete(t *testing.T) {
 }
 
 func TestExamplesComplete2(t *testing.T) {
-	output := RunE2ETest(t, "../../", "examples/complete2", terraform.Options{
+	output := test_helper.RunE2ETest(t, "../../", "examples/complete2", terraform.Options{
 		Upgrade: true,
 	})
 
 	privateSubnetId, ok := output["private_subnet_id"].(string)
 	assert.True(t, ok)
 	assert.NotEqual(t, "", privateSubnetId)
-}
-
-type TerraformOutput = map[string]interface{}
-
-func RunE2ETest(t *testing.T, rootFolder, moduleFolderRelativeToRoot string, option terraform.Options) TerraformOutput {
-	terraformDir := test_structure.CopyTerraformFolderToTemp(t, rootFolder, moduleFolderRelativeToRoot)
-	option.TerraformDir = terraformDir
-	defer terraform.Destroy(t, &option)
-	terraform.InitAndApplyAndIdempotent(t, &option)
-	return terraform.OutputAll(t, &option)
 }
