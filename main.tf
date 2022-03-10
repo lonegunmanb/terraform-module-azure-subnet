53e7691a8d89aa06595303505035da086abd5b6f
zjhe <hezijie@microsoft.com>
2022-03-10 15:19:57 +0800

    fix tffmt issue

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "unused_variable" {
 }
 
 resource "azurerm_subnet" "this" {
-  name                  = var.subnet_name
+  name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network.name
   address_prefixes     = var.address_prefixes
