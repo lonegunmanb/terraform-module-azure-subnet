a093af7c1c9811dca93059d671269561092c43f6
zjhe <hezijie@microsoft.com>
2022-03-10 15:12:49 +0800

    break the ci

--- main.tf
+++ main.tf
@@ -2,8 +2,12 @@
 #
 #}
 
+variable "unused_variable" {
+  default = null
+}
+
 resource "azurerm_subnet" "this" {
-  name                 = var.subnet_name
+  name                  = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network.name
   address_prefixes     = var.address_prefixes
