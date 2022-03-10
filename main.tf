c136d0e13f5fbf8b4f865a66d371f8fa19445449
zjhe <hezijie@microsoft.com>
2022-03-10 09:04:02 +0800

    this shoud fail the pipeline

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #}
 
 resource "azurerm_subnet" "this" {
-  name                 = var.subnet_name
+  name                  = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network.name
   address_prefixes     = var.address_prefixes
