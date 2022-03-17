1f2018a29e840c6c7aa55f7b28b10eda379986cf
zjhe <hezijie@microsoft.com>
2022-03-17 18:54:41 +0800

    add cpu and memory options

--- scripts/run-golangci-lint.sh
+++ scripts/run-golangci-lint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 echo "==> Checking test go code against linters..."
-cd ./test && go mod tidy && golangci-lint run --timeout 1h ./...
+cd ./test
+go mod tidy
+echo "run golangci-lint ..."
+golangci-lint run --timeout 1h ./...
