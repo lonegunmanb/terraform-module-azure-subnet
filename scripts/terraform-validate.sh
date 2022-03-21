c10a0e49af1e024b846bfe49bac6248eeeee59c9
zjhe <hezijie@microsoft.com>
2022-03-21 17:49:08 +0800

    fix terraform validate script

--- scripts/terraform-validate.sh
+++ scripts/terraform-validate.sh
@@ -2,11 +2,11 @@
 
 echo "==> Validating module terraform codes..."
 error=false
-terraform init -upgrade && terraform validate || error=true
+(terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
 echo "==> Checking examples terraform codes are validate..."
 examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
 for d in $examples; do
-  (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && terraform init -upgrade && terraform validate  || error=true)
+  (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
 done
 
 
