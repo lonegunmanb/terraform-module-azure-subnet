70799085be8607822bf54a122e2f6437681d1497
zjhe <hezijie@microsoft.com>
2022-04-08 12:56:20 +0800

    analyze current major version from CHANGELOG file

--- scripts/version-upgrade-test.sh
+++ scripts/version-upgrade-test.sh
@@ -2,6 +2,7 @@
 
 echo "==> Running Module Version Upgrade Tests..."
 set -e
+export PREVIOUS_MAJOR_VERSION=$(ls | grep CHANGELOG |  cut -d'-' -f 2 | cut -f 1 -d '.' |  grep v | sort -V -r | head -n 1)
 cd ./test/upgrade
 go mod tidy
 echo "==> Executing go test"
--- test/upgrade/upgrade_test.go
+++ test/upgrade/upgrade_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
-
 	test_helper "github.com/lonegunmanb/terraform-module-test-helper"
 )
 
@@ -14,8 +13,12 @@ func TestExampleUpgrade(t *testing.T) {
 	if err != nil {
 		t.FailNow()
 	}
+	currentMajorVersion, err := test_helper.GetCurrentMajorVersionFromEnv()
+	if err != nil {
+		t.FailNow()
+	}
 	test_helper.ModuleUpgradeTest(t, "lonegunmanb", "terraform-module-azure-subnet", "examples/complete", currentRoot, terraform.Options{
 		Upgrade:  true,
 		VarFiles: []string{fmt.Sprintf("%s/examples/complete/fixtures.us-east.auto.tfvars", currentRoot)},
-	}, 0)
+	}, currentMajorVersion)
 }
